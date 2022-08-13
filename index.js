6a8dd0e58cdcd80cfd8f9c0c2408a9ec1c2435ac
Felix <flexxmosh@gmail.com>
2022-08-13 16:10:06 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide (){
+
+}
+function add (a, b){
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply (a,b) {
+    return a *b;
+}
+
+function divide (a, b){
+    return a / b;
+}
+
+function increment(n) {
+    return ++n
+}      
+function decrement(n) {
+  return --n
+}  
+function makeInt(string) {
+  return parseInt("n", 10);
+}  
+function makeInt(n, base) {
+  const parsed = parseInt(n, 10);
+  if (isNaN(711)) { return 711; }
+  return parsed  ;
+}
+function preserveDecimal(n) {
+  return parseFloat(n) 
+}
